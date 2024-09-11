339dcd63293fbcaea77655e8065a79e4350e6040
olimov Amir <olimov.amir@mail.ru>
2024-09-11 07:20:56 +0300

    operatorsJS

---
+++ app/app.js
@@ -0,0 +1,72 @@
+/*
+var  age = 17;
+let  firstName = "Farhod";
+const lastName = "Soliev";
+
+let isVisit = true;
+let Visit = false;
+
+
+console.log(typeof(isVisit));
+console.log(typeof(age));
+console.log(typeof(firstName));
+
+
+console.log("--------------------------------------------------------------------------")
+console.log(Visit);
+
+
+
+//const lastName = "SolievHON";  выдаст ошибку 
+//console.log("Переменные var = age: " + age);
+
+
+//console.log("Переменные let = firstName: "  + firstName);
+console.log("Переменные const = lastName: " + lastName);
+var  age = 29;
+firstName = "Farhod 2";
+
+
+//console.log("Переменные var = age: " + age);
+//console.log("Переменные let = firstName: "  + firstName);
+console.log("Переменные const = lastName: " + lastName);
+//console.log("Переменные const = lastName2: " + lastName2);
+
+*/
+let nameRashid = "Rashid"
+
+
+//alert("Hello " + nameRashid);
+
+//let textPromp = prompt("сколько вам лет");
+
+//console.log(textPromp);
+//console.log((textPromp));
+
+//const userConfirmed = confirm("Вы закончили курс по JS"); 
+//console.log(userConfirmed)
+
+
+
+if (userConfirmed == true) {
+    console.log("Вы приняты на работы")
+}else{
+    console.log("Как закончите, затем приходите к нам")
+}
+
+
+
+let age = "19";
+
+if (age == "18") {
+    console.log("Дадоткади биё")
+} else if(age == "19"){
+    console.log("Кандата зан")
+}else{
+    console.log("шуд гапа кам кун")
+}
+
+
+
+
+
---
+++ operatorsJS/app.js
@@ -0,0 +1,57 @@
+//let textpromp = prompt ("привет")
+//console.log(textpromp)
+
+
+/*let user = confirm("are you 18")
+
+if (user == true) {
+    console.log("welcome")
+}
+else {
+    console.log("Вам ещё рано")
+}*/
+
+/*
+let user = prompt ("сколько тебе лет")
+console.log(typeof(user))
+
+console.log(user);
+if (user == 18){
+    alert("Добро пожаловать")
+}
+else if(user < 18){
+    alert("Холи тува бавакт")
+}
+
+*/
+
+
+
+// Арифметические операторы  + - * / %
+// + сложение 
+// - вычитание
+// * умножение
+// / деление
+// % остаток от деление
+
+let sum1 = 5;
+let sum2 = 2;
+
+let result = sum1 % sum2
+
+console.log(result);
+
+
+let x = 5**4;
+
+let y = "10"
+
+
+console.log(x); 
+
+//console.log(y);
+if(y >= 9){
+    console.log("раками 9-ро навис")
+}else{
+    console.log("ин раками 9")
+}
